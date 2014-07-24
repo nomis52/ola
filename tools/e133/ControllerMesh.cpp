cd00d1a32db07db2dbed7b2b7d4de34f360d2abd
Simon Newton <nomis52@gmail.com>
2014-07-24 11:33:14 +0100

    Setup a basic controller mesh.

---
+++ tools/e133/ControllerMesh.cpp
@@ -0,0 +1,506 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * ControllerMesh.cpp
+ * Copyright (C) 2014 Simon Newton
+ */
+
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "ola/Callback.h"
+#include "ola/Logging.h"
+#include "ola/acn/ACNVectors.h"
+#include "ola/io/SelectServerInterface.h"
+#include "ola/network/HealthCheckedConnection.h"
+#include "ola/stl/STLUtils.h"
+#include "ola/network/IPV4Address.h"
+#include "ola/network/SocketAddress.h"
+#include "ola/rdm/RDMCommand.h"
+#include "ola/rdm/RDMCommandSerializer.h"
+#include "plugins/e131/e131/E133Header.h"
+#include "plugins/e131/e131/E133StatusInflator.h"
+#include "plugins/e131/e131/RDMPDU.h"
+#include "tools/e133/ControllerMesh.h"
+#include "tools/e133/E133HealthCheckedConnection.h"
+#include "tools/e133/TCPConnectionStats.h"
+
+using ola::NewCallback;
+using ola::NewSingleCallback;
+using ola::TimeInterval;
+using ola::io::IOStack;
+using ola::network::HealthCheckedConnection;
+using ola::network::IPV4Address;
+using ola::network::IPV4SocketAddress;
+using ola::network::TCPSocket;
+using ola::plugin::e131::TransportHeader;
+using std::auto_ptr;
+using std::string;
+using std::vector;
+
+
+// The max number of un-ack'ed messages we'll allow.
+const unsigned int ControllerMesh::MAX_QUEUE_SIZE = 10;
+
+const unsigned int ControllerMesh::TCP_CONNECT_TIMEOUT_SECONDS = 5;
+const int16_t ControllerMesh::CONNECT_FAILURE_PENALTY = 200;
+
+// retry TCP connects after 5 seconds
+const TimeInterval ControllerMesh::INITIAL_TCP_RETRY_DELAY(5, 0);
+// we grow the retry interval to a max of 30 seconds
+const TimeInterval ControllerMesh::MAX_TCP_RETRY_DELAY(30, 0);
+
+/**
+ * Handles the health checked connection to a controller
+ */
+class ControllerConnection {
+ public:
+  typedef ola::Callback1<void, const IPV4SocketAddress &> CloseCallback;
+
+  ControllerConnection(const ola::network::IPV4SocketAddress &address,
+                       ola::io::SelectServerInterface *ss,
+                       CloseCallback *close_callback,
+                       ola::plugin::e131::E133Inflator *m_e133_inflator)
+      : m_address(address),
+        m_ss(ss),
+        m_close_callback(close_callback),
+        m_root_inflator(
+            NewCallback(this, &ControllerConnection::RLPDataReceived)) {
+    m_root_inflator.AddInflator(m_e133_inflator);
+  }
+
+  ~ControllerConnection() {
+    if (m_tcp_socket.get()) {
+      CloseConnection();
+    }
+  }
+
+  const IPV4SocketAddress& Address() const {
+    return m_address;
+  }
+
+  bool IsConnected() const {
+    return m_tcp_socket.get() != NULL;
+  }
+
+  /**
+   * @param socket the new TCPSocket, ownership is transferred.
+   */
+  bool SetupConnection(TCPSocket *socket_ptr,
+                       ola::e133::MessageBuilder *message_builder) {
+    if (m_tcp_socket.get()) {
+      OLA_WARN << "Already got a TCP connection open, closing the new one";
+      delete socket_ptr;
+      return false;
+    }
+
+    m_tcp_socket.reset(socket_ptr);
+
+    if (m_message_queue.get())
+      OLA_WARN << "Already have a MessageQueue";
+    m_message_queue.reset(new MessageQueue(m_tcp_socket.get(), m_ss,
+                                           message_builder->pool()));
+
+    if (m_health_checked_connection.get()) {
+      OLA_WARN << "Already have a E133HealthCheckedConnection";
+    }
+    m_health_checked_connection.reset(new E133HealthCheckedConnection(
+      message_builder,
+      m_message_queue.get(),
+      ola::NewSingleCallback(this, &ControllerConnection::ConnectionUnhealthy),
+      m_ss));
+
+    // this sends a heartbeat message to indicate this is the live connection
+    if (!m_health_checked_connection->Setup()) {
+      OLA_WARN << "Failed to setup HealthCheckedConnection, closing TCP socket";
+      m_health_checked_connection.reset();
+      m_message_queue.reset();
+      m_tcp_socket.reset();
+      return false;
+    }
+
+    // TODO(simon): Send the first PDU here that contains our IP:Port:UID info.
+
+    if (m_incoming_tcp_transport.get()) {
+      OLA_WARN << "Already have an IncomingTCPTransport";
+    }
+    m_incoming_tcp_transport.reset(new ola::plugin::e131::IncomingTCPTransport(
+        &m_root_inflator, m_tcp_socket.get()));
+
+    m_tcp_socket->SetOnData(
+      NewCallback(this, &ControllerConnection::ReceiveTCPData));
+    m_tcp_socket->SetOnClose(
+      NewSingleCallback(this, &ControllerConnection::CloseConnection));
+    m_ss->AddReadDescriptor(m_tcp_socket.get());
+    return true;
+  }
+
+  bool seen;  // used to remove old controllers
+
+ private:
+  ola::network::IPV4SocketAddress m_address;
+  ola::io::SelectServerInterface *m_ss;
+  CloseCallback *m_close_callback;
+  ola::plugin::e131::RootInflator m_root_inflator;
+
+  auto_ptr<TCPSocket> m_tcp_socket;
+  auto_ptr<E133HealthCheckedConnection> m_health_checked_connection;
+  auto_ptr<MessageQueue> m_message_queue;
+  auto_ptr<ola::plugin::e131::IncomingTCPTransport> m_incoming_tcp_transport;
+
+  void CloseConnection() {
+    OLA_INFO << "Closing TCP conection to " << m_address;
+    m_ss->RemoveReadDescriptor(m_tcp_socket.get());
+
+    // shutdown the tx side
+    m_health_checked_connection.reset();
+    m_message_queue.reset();
+    m_incoming_tcp_transport.reset();
+
+    // finally delete the socket
+    m_tcp_socket.reset();
+
+    m_close_callback->Run(m_address);
+  }
+
+  void ConnectionUnhealthy() {
+    OLA_INFO << "Connection to " << m_address << " went unhealthy.";
+    CloseConnection();
+  }
+
+  void ReceiveTCPData() {
+    if (!m_incoming_tcp_transport->Receive()) {
+      OLA_WARN << "TCP stream to " << m_address << " is bad";
+      CloseConnection();
+    }
+  }
+
+  void RLPDataReceived(const TransportHeader&) {
+    if (m_health_checked_connection.get()) {
+      m_health_checked_connection->HeartbeatReceived();
+    }
+  }
+};
+
+
+/**
+ * Create a new ControllerMesh.
+ * This listens for connections from the controllers, and will ensure that if
+ * any controllers try to connect, at least one will be picked as the
+ * designated controller.
+ */
+ControllerMesh::ControllerMesh(
+    RefreshControllersCallback *refresh_controllers_cb,
+    ola::io::SelectServerInterface *ss,
+    ola::e133::MessageBuilder *message_builder,
+    uint16_t our_port,
+    unsigned int max_queue_size)
+    : m_controllers_cb(refresh_controllers_cb),
+      m_our_port(our_port),
+      m_max_queue_size(max_queue_size),
+      m_ss(ss),
+      m_message_builder(message_builder),
+      m_discovery_timeout(ola::thread::INVALID_TIMEOUT),
+      m_tcp_socket_factory(NewCallback(this, &ControllerMesh::OnTCPConnect)),
+      m_tcp_connector(
+          m_ss, &m_tcp_socket_factory,
+          TimeInterval(TCP_CONNECT_TIMEOUT_SECONDS, 0)),
+      m_backoff_policy(INITIAL_TCP_RETRY_DELAY, MAX_TCP_RETRY_DELAY) {
+  m_e133_inflator.AddInflator(&m_e133_status_inflator);
+
+  m_e133_status_inflator.SetStatusHandler(
+      NewCallback(this, &ControllerMesh::HandleStatusMessage));
+}
+
+ControllerMesh::~ControllerMesh() {
+
+  if (m_discovery_timeout != ola::thread::INVALID_TIMEOUT) {
+    m_ss->RemoveTimeout(m_discovery_timeout);
+  }
+
+  ControllerList::iterator iter = m_known_controllers.begin();
+  for (; iter != m_known_controllers.end(); ++iter) {
+    delete *iter;
+  }
+
+  if (m_controllers_cb) {
+    delete m_controllers_cb;
+  }
+}
+
+bool ControllerMesh::Start() {
+  if (!m_controllers_cb) {
+    return false;
+  }
+
+  CheckForNewControllers();
+
+  m_discovery_timeout = m_ss->RegisterRepeatingTimeout(
+      TimeInterval(2, 0),
+      NewCallback(this, &ControllerMesh::CheckForNewControllers));
+  return true;
+}
+
+bool ControllerMesh::CheckForNewControllers() {
+  vector<IPV4SocketAddress> controllers;
+  m_controllers_cb->Run(&controllers);
+  // OLA_INFO << "I know about " << controllers.size() << " controllers";
+
+  ControllerList::iterator iter = m_known_controllers.begin();
+  for (; iter != m_known_controllers.end(); ++iter) {
+    (*iter)->seen = false;
+  }
+
+  vector<IPV4SocketAddress>::const_iterator c_iter = controllers.begin();
+  for (; c_iter != controllers.end(); ++c_iter) {
+    if (c_iter->Host() == IPV4Address::Loopback() &&
+        c_iter->Port() == m_our_port) {
+      // OLA_INFO << "Skipping " << *c_iter << " since it's ourself";
+      continue;
+    }
+
+    ControllerConnection *info = FindControllerConnection(*c_iter);
+    if (!info) {
+      info = new ControllerConnection(
+        *c_iter,
+        m_ss,
+        NewCallback(this, &ControllerMesh::TCPConnectionClosed),
+        &m_e133_inflator);
+      m_known_controllers.push_back(info);
+      m_tcp_connector.AddEndpoint(*c_iter, &m_backoff_policy);
+    }
+    info->seen = true;
+  }
+
+  if (!m_known_controllers.empty()) {
+    OLA_INFO << m_known_controllers.size() << " known controllers";
+  }
+
+  // Remove any controllers that no longer exist.
+  for (iter = m_known_controllers.begin(); iter != m_known_controllers.end();) {
+    if (!(*iter)->seen) {
+      // TODO(simon): handle the case where the connection to the controller is
+      // still open.
+      OLA_INFO << "Removed " << (*iter)->Address();
+      m_tcp_connector.RemoveEndpoint((*iter)->Address());
+      delete *iter;
+      iter = m_known_controllers.erase(iter);
+      continue;
+    }
+    iter++;
+  }
+  return true;
+}
+
+void ControllerMesh::OnTCPConnect(TCPSocket *socket_ptr) {
+  auto_ptr<TCPSocket> socket(socket_ptr);
+
+  ola::network::GenericSocketAddress generic_peer = socket->GetPeerAddress();
+  if (generic_peer.Family() != AF_INET) {
+    OLA_WARN << "Unknown family " << generic_peer.Family();
+    return;
+  }
+  IPV4SocketAddress peer = generic_peer.V4Addr();
+
+  OLA_INFO << "Connected to controller at " << peer;
+
+  ControllerConnection *info = FindControllerConnection(peer);
+
+  if (!info) {
+    OLA_WARN << "Can't find controller for " << peer;
+    return;
+  }
+
+  info->SetupConnection(socket.release(), m_message_builder);
+  return;
+
+  /*
+  if (info->m_tcp_socket) {
+    OLA_WARN << "Already got a TCP connection open, closing the new one";
+    socket->Close();
+    return;
+  }
+
+  info->m_tcp_socket = socket.release();
+  if (info->m_message_queue)
+    OLA_WARN << "Already have a MessageQueue";
+  info->m_message_queue = new MessageQueue(info->m_tcp_socket, m_ss,
+                                           m_message_builder->pool());
+
+  if (info->m_health_checked_connection) {
+    OLA_WARN << "Already have a E133HealthCheckedConnection";
+  }
+
+  info->m_health_checked_connection = new E133HealthCheckedConnection(
+    m_message_builder,
+    info->m_message_queue,
+    ola::NewSingleCallback(
+      this, &ControllerMesh::TCPConnectionUnhealthy, peer),
+    m_ss);
+
+  // this sends a heartbeat message to indicate this is the live connection
+  if (!info->m_health_checked_connection->Setup()) {
+    OLA_WARN << "Failed to setup HealthCheckedConnection, closing TCP socket";
+    delete info->m_health_checked_connection;
+    info->m_health_checked_connection = NULL;
+    delete info->m_message_queue;
+    info->m_message_queue = NULL;
+    info->m_tcp_socket->Close();
+    delete info->m_tcp_socket;
+    info->m_tcp_socket = NULL;
+    return;
+  }
+
+  // TODO(simon): Send the first PDU here that contains our IP:Port:UID info.
+
+  if (info->m_incoming_tcp_transport) {
+    OLA_WARN << "Already have an IncomingTCPTransport";
+  }
+  info->m_incoming_tcp_transport = new ola::plugin::e131::IncomingTCPTransport(
+      &m_root_inflator, info->m_tcp_socket);
+
+  info->m_tcp_socket->SetOnData(
+      NewCallback(this, &ControllerMesh::ReceiveTCPData,
+      info->m_incoming_tcp_transport));
+  info->m_tcp_socket->SetOnClose(ola::NewSingleCallback(
+      this, &ControllerMesh::TCPConnectionClosed, peer));
+  m_ss->AddReadDescriptor(info->m_tcp_socket);
+  */
+}
+
+
+/**
+ * Called when there is new TCP data available
+void ControllerMesh::ReceiveTCPData(
+    ola::plugin::e131::IncomingTCPTransport *incoming_tcp_transport) {
+  if (!incoming_tcp_transport->Receive()) {
+    OLA_WARN << "TCP STREAM IS BAD!!!";
+    //CloseTCPConnection();
+  }
+}
+
+ * Called when the TCP connection goes unhealthy.
+void ControllerMesh::TCPConnectionUnhealthy(IPV4SocketAddress peer_addr) {
+  OLA_INFO << "TCP connection to " << peer_addr << " went unhealthy, closing";
+  ControllerConnection *info = FindControllerConnection(peer_addr);
+
+  (void) info;
+  //CloseTCPConnection();
+}
+*/
+
+void ControllerMesh::TCPConnectionClosed(const IPV4SocketAddress &peer_addr) {
+  m_tcp_connector.Disconnect(peer_addr);
+}
+
+  /*
+  m_ss->RemoveReadDescriptor(info->m_tcp_socket);
+
+  // shutdown the tx side
+  delete info->m_health_checked_connection;
+  info->m_health_checked_connection = NULL;
+
+  delete info->m_message_queue;
+  info->m_message_queue = NULL;
+
+  // shutdown the rx side
+  delete info->m_incoming_tcp_transport;
+  info->m_incoming_tcp_transport = NULL;
+
+  // finally delete the socket
+  info->m_tcp_socket->Close();
+  delete info->m_tcp_socket;
+  info->m_tcp_socket = NULL;
+  */
+
+/**
+ * Called when we receive a valid Root Layer PDU.
+void ControllerMesh::RLPDataReceived(const TransportHeader&) {
+  if (m_health_checked_connection) {
+    m_health_checked_connection->HeartbeatReceived();
+  }
+}
+  */
+
+/*
+bool ControllerMesh::SendRDMCommand(
+    unsigned int sequence_number,
+    uint16_t endpoint,
+    const RDMResponse *rdm_response) {
+  if (m_message_queue->LimitReached())
+    return false;
+
+  IOStack packet(m_message_builder->pool());
+  ola::rdm::RDMCommandSerializer::Write(*rdm_response, &packet);
+  ola::plugin::e131::RDMPDU::PrependPDU(&packet);
+  m_message_builder->BuildTCPRootE133(
+      &packet, ola::acn::VECTOR_FRAMING_RDMNET, sequence_number,
+      endpoint);
+
+  return m_message_queue->SendMessage(&packet);
+}
+*/
+
+
+/**
+ * Handle a E1.33 Status PDU on the TCP connection.
+ */
+void ControllerMesh::HandleStatusMessage(
+    const TransportHeader *transport_header,
+    const ola::plugin::e131::E133Header *e133_header,
+    uint16_t status_code,
+    const string &description) {
+  (void) transport_header;
+  (void) e133_header;
+  (void) status_code;
+  (void) description;
+/*
+  if (status_code != ola::e133::SC_E133_ACK) {
+    OLA_INFO << "Received a non-ack status code from "
+             << transport_header->Source() << ": " << status_code << " : "
+             << description;
+  }
+  OLA_INFO << "Controller has ack'ed " << e133_header->Sequence();
+
+  ola::STLRemoveAndDelete(&m_unacked_messages, e133_header->Sequence());
+  if (m_unsent_messages && !m_message_queue->LimitReached()) {
+    bool sent_all = true;
+    PendingMessageMap::iterator iter = m_unacked_messages.begin();
+    for (; iter != m_unacked_messages.end(); iter++) {
+      OutstandingMessage *message = iter->second;
+      if (message->was_sent())
+        continue;
+      bool was_sent = SendRDMCommand(iter->first, message->endpoint(),
+                                     message->rdm_response());
+      sent_all &= was_sent;
+      message->set_was_sent(was_sent);
+    }
+    m_unsent_messages = !sent_all;
+  }
+*/
+}
+
+ControllerConnection *ControllerMesh::FindControllerConnection(
+    const IPV4SocketAddress &peer_addr) {
+  ControllerList::iterator iter = m_known_controllers.begin();
+  for (; iter != m_known_controllers.end(); ++iter) {
+    if ((*iter)->Address() == peer_addr) {
+      return *iter;
+    }
+  }
+  return NULL;
+}
---
+++ tools/e133/ControllerMesh.h
@@ -0,0 +1,143 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * ControllerMesh.h
+ * Manages the connection between E1.33 controllers.
+ * Copyright (C) 2014 Simon Newton
+ */
+
+#ifndef TOOLS_E133_CONTROLLERMESH_H_
+#define TOOLS_E133_CONTROLLERMESH_H_
+
+#include <stdint.h>
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include "ola/Callback.h"
+#include "ola/base/Macro.h"
+#include "ola/e133/MessageBuilder.h"
+#include "ola/io/SelectServerInterface.h"
+#include "ola/network/AdvancedTCPConnector.h"
+#include "ola/network/TCPSocket.h"
+#include "ola/rdm/RDMCommand.h"
+#include "plugins/e131/e131/E133Inflator.h"
+#include "plugins/e131/e131/E133StatusInflator.h"
+#include "plugins/e131/e131/RootInflator.h"
+#include "tools/e133/E133HealthCheckedConnection.h"
+#include "tools/e133/MessageQueue.h"
+
+
+#include "ola/network/Socket.h"
+#include "ola/network/SocketAddress.h"
+#include "ola/util/SequenceNumber.h"
+#include "plugins/e131/e131/TCPTransport.h"
+
+
+/**
+ * @brief Manages the connection between E1.33 controllers.
+ *
+ * The controller(s) are located by the refesh_controllers callback. This is
+ * usually called when a connection fails, or when periodically as we're trying
+ * to locate controllers.
+ */
+class ControllerMesh {
+ public:
+  /**
+   * @brief The callback run as a result of FindControllers.
+   *
+   * The callback populates the first argument with the list of known
+   * controllers.
+   */
+  typedef ola::Callback1<void, std::vector<ola::network::IPV4SocketAddress>*>
+      RefreshControllersCallback;
+
+  /**
+   * @brief Create a new ControllerMesh.
+   * @param refresh_controllers_cb The callback used to refresh the controller
+   *   list. Ownership is transferred.
+   */
+  ControllerMesh(RefreshControllersCallback *refresh_controllers_cb,
+                 ola::io::SelectServerInterface *ss,
+                 ola::e133::MessageBuilder *message_builder,
+                 uint16_t our_port,
+                 unsigned int max_queue_size = MAX_QUEUE_SIZE);
+
+  /**
+   * @Clean up
+   */
+  ~ControllerMesh();
+
+  /**
+   * @brief Start trying to connect to all E1.33 controllers.
+   */
+  bool Start();
+
+ private:
+  typedef std::vector<class ControllerConnection*> ControllerList;
+
+  RefreshControllersCallback *m_controllers_cb;
+  const uint16_t m_our_port;
+  const unsigned int m_max_queue_size;
+  ola::io::SelectServerInterface *m_ss;
+  ola::e133::MessageBuilder *m_message_builder;
+
+  // Connection members
+  ControllerList m_known_controllers;
+  ola::thread::timeout_id m_discovery_timeout;
+  ola::network::TCPSocketFactory m_tcp_socket_factory;
+  ola::network::AdvancedTCPConnector m_tcp_connector;
+  ola::LinearBackoffPolicy m_backoff_policy;
+
+  // Inflators
+  ola::plugin::e131::E133Inflator m_e133_inflator;
+  ola::plugin::e131::E133StatusInflator m_e133_status_inflator;
+
+  bool CheckForNewControllers();
+  void OnTCPConnect(ola::network::TCPSocket *socket);
+
+  /*
+  void ReceiveTCPData(
+    ola::plugin::e131::IncomingTCPTransport *m_incoming_tcp_transport);
+  void TCPConnectionUnhealthy(ola::network::IPV4SocketAddress peer_addr);
+  */
+  void TCPConnectionClosed(const ola::network::IPV4SocketAddress &peer_addr);
+  //void RLPDataReceived(const ola::plugin::e131::TransportHeader &header);
+
+  /*
+  bool SendRDMCommand(unsigned int sequence_number, uint16_t endpoint,
+                      const ola::rdm::RDMResponse *rdm_response);
+  */
+
+  void HandleStatusMessage(
+      const ola::plugin::e131::TransportHeader *transport_header,
+      const ola::plugin::e131::E133Header *e133_header,
+      uint16_t status_code,
+      const std::string &description);
+
+  class ControllerConnection *FindControllerConnection(
+      const ola::network::IPV4SocketAddress &peer_addr);
+
+  static const unsigned int MAX_QUEUE_SIZE;
+  static const unsigned int TCP_CONNECT_TIMEOUT_SECONDS;
+  static const int16_t CONNECT_FAILURE_PENALTY;
+
+  static const ola::TimeInterval INITIAL_TCP_RETRY_DELAY;
+  static const ola::TimeInterval MAX_TCP_RETRY_DELAY;
+
+  DISALLOW_COPY_AND_ASSIGN(ControllerMesh);
+};
+#endif  // TOOLS_E133_CONTROLLERMESH_H_
